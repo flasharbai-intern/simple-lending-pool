f532bebb2a3511e8a8fdad22f00b1b06ab79fdf4
flasharbai-intern <flasharbai@gmail.com>
2025-08-21 10:30:02 -0400

    Feat: Add Borrow Event

--- src/LendingPool.sol
+++ src/LendingPool.sol
@@ -56,5 +56,6 @@ contract LendingPool is ILendingPool, Ownable, ReentrancyGuard {
     event Withdraw(address indexed user, uint256 amount, uint256 lpTokens);
     event DepositCollateral(address indexed user, uint256 amount);
     event WithdrawCollateral(address indexed user, uint256 amount);
+    event Borrow(address indexed user, uint256 amount);
 
 }
