c2814555371cad58ca380171e70e19e2165f1018
flasharbai-intern <flasharbai@gmail.com>
2025-08-25 00:59:16 -0400

    Feat: Add LendingPoolToken Contract Title Natspec Comments

--- src/LendingPoolToken.sol
+++ src/LendingPoolToken.sol
@@ -1,4 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+/**
+ * @title LendingPoolToken
+ * @notice ERC20 token representing shares in the lending pool
+ * @dev Only the lending pool contract can mint and burn tokens
+ */
 contract LendingPoolToken  {}
