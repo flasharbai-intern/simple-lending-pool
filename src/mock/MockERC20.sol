9c415f09071f5b710d291d9e0279fb5f853d4eda
flasharbai-intern <flasharbai@gmail.com>
2025-09-14 18:34:01 -0700

    feat: Add Title Natspec Comments For Contract

--- src/mock/MockERC20.sol
+++ src/mock/MockERC20.sol
@@ -1,4 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+/**
+ * @title MockERC20
+ * @notice Simple ERC20 token for testing purposes
+ * @dev Allows anyone to mint tokens for testing
+ */
 contract MockERC20 {}
