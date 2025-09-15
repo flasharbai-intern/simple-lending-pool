a576c7e9ce07b87de9845edfd299d16835b836d5
flasharbai-intern <flasharbai@gmail.com>
2025-09-14 18:35:02 -0700

    feat: Add Necessary Library Import

--- src/mock/MockERC20.sol
+++ src/mock/MockERC20.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 /**
  * @title MockERC20
  * @notice Simple ERC20 token for testing purposes
