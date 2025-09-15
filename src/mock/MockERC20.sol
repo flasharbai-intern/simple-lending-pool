d4db3e0e0b595e4cb3546fef01ab7efcdda161de
flasharbai-intern <flasharbai@gmail.com>
2025-09-14 18:35:59 -0700

    feat: Add Constructor Function

--- src/mock/MockERC20.sol
+++ src/mock/MockERC20.sol
@@ -8,4 +8,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
  * @notice Simple ERC20 token for testing purposes
  * @dev Allows anyone to mint tokens for testing
  */
-contract MockERC20 is ERC20 {}
+contract MockERC20 is ERC20 {
+    constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
+        _mint(msg.sender, initialSupply);
+    }
+}
